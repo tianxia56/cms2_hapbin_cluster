e0c9af554d4b4e02adc95e554e8cf67e8612d4c5
tianxia56 <tian.xia.tx56@yale.edu>
2025-01-12 14:53:07 -0500

    Update run_ihs_neut.sh

--- run_ihs_neut.sh
+++ run_ihs_neut.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=2-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8000
 
 # Read inputs from JSON file using Python
