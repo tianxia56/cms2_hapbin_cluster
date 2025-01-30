86edba6c254f8a57ef58c4432de482ab889efcff
tianxia56 <tian.xia.tx56@yale.edu>
2025-01-30 14:46:31 -0500

    Add files via upload

--- clean.sh
+++ clean.sh
@@ -12,9 +12,9 @@ rm one_pop_stats/*
 rm two_pop_stats/*
 rm norm/*
 rm hapbin/*
-rm output/*.tsv
 rm slurm-*
 rm runtime/*
 rm bin/*
+rm output/*all*
 
 #for id in {46295909..46295915}; do scancel $id; done
